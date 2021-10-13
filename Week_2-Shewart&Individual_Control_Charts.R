b21efe5dbd8e805377a426285fb4a04a4fd1f4d9
Michael Lappert <mi.lappert@gmail.com>
2021-10-13 17:05:53 +0200

    Week 2 initial commit

---
+++ Week_2-Shewart&Individual_Control_Charts.R
@@ -0,0 +1,24 @@
+################################################################################
+# AppStat Exercises from week 3
+################################################################################
+# Exercises from the script
+#------------------------------------------------------------------------------
+setwd("~/OneDrive/Learning/MSE/2021hs/FTP_AppStat/Additional_material_AppStat/04 Datasets")
+
+#*******************************************************************************
+# Problem 3.6.1 (Western Electric Rules)
+#* Examine if the process of Ex. 2.2.1 is under control according to the 
+#* Western Electric Rules. Just look at the chart for ¯x which is based on 
+#* the chart for R.
+#*******************************************************************************
+file <- "ignition-keys.dat"
+df <- read.table(file, header = TRUE)
+head(df)
+summary(df)
+str(df)
+
+m <- length(df); m
+df$mean <- apply(df[,1:m], 1, mean)
+df$sd <- apply(df[,1:m], 1, sd)
+df$R <- apply(df[,1:m], 1, function(x){ max(x) - min(x) })
+str(df)
