d871e8f34057e5a28b2fbee8a9aea196124a440b
Michael Lappert <mi.lappert@gmail.com>
2021-10-29 20:00:27 +0200

    3.6.1

--- Week_3-Statistical_Properties_of_Control_Charts.R
+++ Week_3-Statistical_Properties_of_Control_Charts.R
@@ -53,11 +53,47 @@ x.barbar
 
 # With these estimates one can calculate the control limits of the x.bar chart
 n <- 4
-x.bar.CL <- x.barbar + c(-3, 0, 3) * sigma.R.hat / sqrt(n); x.bar.CL
+x.bar.CL <- x.barbar + c(-3, -2, -1, 0, 1, 2, 3) * sigma.R.hat / sqrt(n); x.bar.CL
 
 # Now draw the x.bar plot with its centrelines
 plot(df$mean, pch = 20, ylim = c(0.19, 0.21),
      ylab = "Mean Values", main = "x.bar Chart (based on R chart)")
 lines(df$mean)
-abline(h = x.bar.CL, lty = c(2, 1, 2))
+abline(h = x.bar.CL, lty = c(2, 3, 3, 1, 3, 3, 2))
 text(rep(1, 1), x.bar.CL, labels = c("LCL", "CL", "UCL"), pos = 3)
+
+#* The wester electric rules for monitoring control charts are defined as 
+#* follows:
+#* The process is out of control if any of the following applies
+#* 1. Any single data point falls outside the limit defined by UCL and LCL
+#*    (beyond the 3*sigma-limit)
+#* 2. Two out of three consecutive points fall beyond the limit defined by 
+#*    2/3 UCL and 2/3 LCL on the same side of the centreline (beyond the 
+#*    2*sigma-limit)
+#* 3. Four out of five consecutive points fall beyond the limit defined by 
+#*    1/3 UCL and 1/3 LCL on the same side of the centreline (beyond the 
+#*    2*sigma-limit)
+#* 4. Nine consecutive points fall on the same side of the centreline 
+#*    (so-called run)
+#*    
+#*    NOTE for created chart:
+#* 1. No data point falls outside the limit defined by UCL and LCL
+#* 2. No observation where 2 out of 3 consecutive points fall beyond the limit
+#*    defined by the 2/3 UCL or LCL
+#* 3. No observations where 4 out of 5 consecutive points fall beyond the limit
+#*    defined by the 1/3 UCL or LCL
+#* 4. No observation where 9 consecutive points fal beyond the same side of the 
+#*    centreline
+#*    
+#* CONCLUSION: According to the western electric rules the process is 
+#*              under control
+
+
+#*******************************************************************************
+# Problem 3.6.2 (OC Curve)
+#* Determine an approximate operating characteristic curve of the chart for 
+#* x.bar (based on the s chart) of Ex. 2.2.1. The target value µ_0 and the
+#* process standard deviation σ are unknown and can be estimated from the data.
+#* Calculate the probability of an omitted alarm if 
+#* ∆µ = µ_1 − µ.bar_0 = δ σ.bar = 0.015 cm.
+#*******************************************************************************
