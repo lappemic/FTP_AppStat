b40c56da2f7e742efcea6399e816f46b24f921d7
Michael Lappert <mi.lappert@gmail.com>
2021-11-03 12:29:02 +0100

    4.3.1

--- Week_4-Control_Charts_with_Memory.R
+++ Week_4-Control_Charts_with_Memory.R
@@ -18,3 +18,45 @@ df <- read.table(path, header = TRUE)
 head(df)
 summary(df)
 str(df)
+
+# target value
+mu0 <- 10.0
+
+# Process standard deviation using the standard deviation of the data
+sigma.hat <- sd(df$diam); sigma.hat
+
+# Reference value and the decision interval form the process standard deviation
+# Reference value:
+K <- 0.5 * sigma.hat; K
+
+# Decision interval:
+H <- 5 * sigma.hat; H
+
+# Calculation of the recursions C^+ and C^- for the CUSUMs
+# C^+ and C^- CUSUMs
+C.plus <- NULL
+C.plus[1] <- 0
+
+C.minus <- NULL
+C.minus[1] <- 0
+
+n <- length(df$diam); n
+
+for(i in 1:n) {
+  C.plus[i+1] <- max(0, df$diam[i] - (mu0+K) + C.plus[i])
+  C.minus[i+1] <- max(0, (mu0-K) - df$diam[i] + C.minus[i])
+}
+C.plus
+C.minus
+
+# Plot the CUSUM chart
+plot(0:n, C.plus, col = "black", pch = 20, ylim = c(-1, 1),
+     xlab = "Index", ylab = "Cumulative Sums", main = "CUSUM")
+lines(0:n, C.plus, col = "black")
+points(0:n, -C.minus, pch = 20, col = "gray")
+lines(0:n, -C.minus, col = "gray")
+abline(h = c(-1, 0, 1) * H, lty = c(2, 1, 2))
+text(rep(2, 3), c(-1,1) * H, label = c("Lower CUSUM", "Upper CUSUM"), pos = 3)
+#   NOTE: The process is under control. But we recognise something going
+#         on in the last 4 measurements (increasing trend). So we have to 
+#         watch the process closely in the next few measurements
