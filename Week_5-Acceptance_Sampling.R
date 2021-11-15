6585ff2d730eda0f220f7bfbc5d45a03bac9f4d7
Michael Lappert <mi.lappert@gmail.com>
2021-11-15 21:53:04 +0100

    inititalizing week 5 - Problemnr. correction

--- Week_5-Acceptance_Sampling.R
+++ Week_5-Acceptance_Sampling.R
@@ -2,14 +2,16 @@
 # AppStat Exercises from week 5
 ################################################################################
 # Exercises from the script
-#------------------------------------------------------------------------------
+#*------------------------------------------------------------------------------
 
 rm(list=ls(all=TRUE))
 
 #*******************************************************************************
-#* Problem 4.3.1 (CUSUM of Individual Measurements)
-#* The diameter of diﬀerent holes with the same nominal size is regularly
-#* measured automatically. You can ﬁnd the data in diameter.dat 
-#* (cf. Prob. 2.3.1). Examine if the process is under control with a CUSUM chart. 
-#* The target value is µ 0 = 10.0. 
+#* Problem 5.3.1 (Hypergeometric Distribution I) 
+#* Four out of ten lots win. Determine the probability that out of ﬁve randomly
+#*  selected lots there will be
 #*******************************************************************************
+#* a. exactly two winning lots.
+#* b. a maximum of three winning lots.
+#* c. more than one winning lot. 
+#*------------------------------------------------------------------------------
