f59d2d03b777500181e1e244c006243f5eac90b2
Michael Lappert <mi.lappert@gmail.com>
2021-11-15 21:52:13 +0100

    Initializing week 5

---
+++ Week_5-Acceptance_Sampling.R
@@ -0,0 +1,15 @@
+################################################################################
+# AppStat Exercises from week 5
+################################################################################
+# Exercises from the script
+#------------------------------------------------------------------------------
+
+rm(list=ls(all=TRUE))
+
+#*******************************************************************************
+#* Problem 4.3.1 (CUSUM of Individual Measurements)
+#* The diameter of diﬀerent holes with the same nominal size is regularly
+#* measured automatically. You can ﬁnd the data in diameter.dat 
+#* (cf. Prob. 2.3.1). Examine if the process is under control with a CUSUM chart. 
+#* The target value is µ 0 = 10.0. 
+#*******************************************************************************
