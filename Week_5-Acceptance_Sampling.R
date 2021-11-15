8f523c02dcbfd81a6e34dc2d331df1e60bdb0fd4
Michael Lappert <mi.lappert@gmail.com>
2021-11-16 00:02:08 +0100

    5.3.2

--- Week_5-Acceptance_Sampling.R
+++ Week_5-Acceptance_Sampling.R
@@ -47,6 +47,33 @@ c <- 1            # acceptance number
 phyper(q = c, m = p*N, n = (1-p)*N, k = n, lower.tail = FALSE)
 
 
+#*******************************************************************************
+#* Problem 5.3.2 (Hypergeometric Distribution II) 
+#* There are N = m+n balls in an urn, of which m are red and n balls are black.
+#* We draw k balls at once. Determine the probability that there are exactly 
+#* x red balls among the k balls.
+#*******************************************************************************
+#* There are
+#*    (m+n)
+#*    ( k )
+#* possibilities in total to arrangke k balls among m+n balls, and exactly
+#*    (m)( n )
+#*    (x)(k-x)
+#* possibilities to arrange x red balls among k balls. The factor
+#*    (m)
+#*    (x)
+#* counts the number of arrangements with x red balls among m red balls, 
+#* and the factor
+#*    ( n )
+#*    (k-x)
+#* counts the number of arrangements with k-x black balls among n black balls. 
+#* Therefore we obtain
+#*            (m)( n )
+#*            (x)(k-x)
+#*            ---------
+#* P(X = x) =   (m+n)
+#*              ( k )
+
 source("Functions-summary-AppStat.R")
 operating_characteristic_function()
 
