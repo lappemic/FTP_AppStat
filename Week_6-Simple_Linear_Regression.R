165630ead7d8727b967e6c11fb37b27284a41e37
Michael Lappert <mi.lappert@gmail.com>
2021-12-08 12:24:06 +0100

    7.5.2

--- Week_6-Simple_Linear_Regression.R
+++ Week_6-Simple_Linear_Regression.R
@@ -141,3 +141,132 @@ diff(range(df$Year)) * beta1.hat
 #--------------------------------------------------------------------------------------------------------------------------------------------------
 summary(mod)
 
+
+
+#*******************************************************************************
+#* Problem 7.5.2 (Windmill, Example 5.2)
+#* A research engineer is investigating the use of a windmill to generate 
+#* electricity. She has collected data on DC output [in A] from her windmill and
+#* the corresponding wind velocity [in m/s]. The data set windmill.dat contains 
+#* the wind velocities and the DC outputs.
+#*******************************************************************************
+#* a. Plot a scatter diagram of DC output versus wind velocity and a scatter 
+#*    diagram of DC output versus the inverse wind velocity. What are your 
+#*    observations?
+#* b. Fit the model
+#*    
+#*    DCOutput(WindVelocity) = β_0 + (β_1/WindVelocity)
+#*    
+#*    to the data. Give the estimated parameter values and the standard errors.
+#* c. Calculate the 99% conﬁdence interval on β_1.
+#* d. Add the best model to the scatter diagram DC output versus the inverse 
+#*    wind velocity.
+#*    Give a physical interpretation of the parameters β_0 and β_1.
+#* e. Calculate the estimated DC output, the 95% conﬁdence interval of the 
+#*    response and the 95% prediction interval with a wind velocity of one and 
+#*    ten metres per second. Comment your results.
+#*------------------------------------------------------------------------------
+#* Read the data
+path <- file.path("04_Datasets", "windmill.dat")
+df <- read.table(path, header = TRUE)
+head(df)
+str(df)
+
+#*------------------------------------------------------------------------------
+#* a. Plot a scatter diagram of DC output versus wind velocity and a scatter 
+#*    diagram of DC output versus the inverse wind velocity. What are your 
+#*    observations?
+#*------------------------------------------------------------------------------
+df$WindVelocity.inv <- 1/df$WindVelocity
+
+par(mfrow=c(1,2))
+plot(df$DCOutput ~ df$WindVelocity)
+plot(df$DCOutput ~ df$WindVelocity.inv)
+
+#   NOTE: The correlation is much more obvious in the inverted plot
+
+#*------------------------------------------------------------------------------
+#* b. Fit the model to the data. 
+#*    Give the estimated parameter values and the standard errors.
+#*------------------------------------------------------------------------------
+
+mod <- lm(df$DCOutput ~ df$WindVelocity.inv)
+
+# Parameter values and standard errors
+summary(mod)
+
+# Or accessed directly
+## Parameter values (beta_0 and beta_1)
+mod$coefficients
+
+## Standard errors
+summary(mod)$coefficients[,2]
+
+#*------------------------------------------------------------------------------
+#* c. Calculate the 99% conﬁdence interval on β_1.
+#*------------------------------------------------------------------------------
+confint(mod, level = 0.99)
+
+# Or beta_1 accessed directly:
+confint(mod, parm = 2, level = 0.99)
+
+#*------------------------------------------------------------------------------
+#* d. Add the best model to the scatter diagram DC output versus the inverse 
+#*    wind velocity.
+#*    Give a physical interpretation of the parameters β_0 and β_1.
+#*------------------------------------------------------------------------------
+par(mfrow=c(1,2))
+plot(df$DCOutput ~ df$WindVelocity.inv)
+abline(mod)
+grid()
+
+#   original variable
+WV.new <- data.frame(WindVelocity.inv=seq(0.2*min(1/df$WindVelocity), 
+                                          2*max(1/data$WindVelocity), 
+                                          length=101))
+DCOutput.pred <- predict(mod.inv, newdata=WV.new)
+plot(DCOutput ~ WindVelocity, df, pch=20, xlim=c(0,50), ylim=c(0,3), 
+     main="DCOutput versus WindVelocity with best model")
+grid()
+abline(h=0)
+
+#   add best model
+lines(1/WV.new$WindVelocity.inv, DCOutput.pred)
+
+#   add maximum possible DC output
+abline(h=mod.inv$coefficients["(Intercept)"], col="blue")
+
+#   minimal wind velocity to produce DC output, 
+#   i.e. solve DCOutput(WindVelocity)=0 for WindVelocity
+WV.min <- -as.numeric(mod.inv$coefficients["WindVelocity.inv"] / 
+                        mod.inv$coefficients["(Intercept)"]);   WV.min
+abline(v=WV.min, col="red")
+
+#   REMARKS: The best model fits nicely.
+#            If the wind velocity were to tend to infinity then the parameter beta0 would be the maximum possible DC output, that is 2.97886 A.
+#            Observing the diagram we can see that a certain minimal wind velocity is necessary to produce a DC output, that is 5.208521 m/s.
+
+#*------------------------------------------------------------------------------
+#* e. Calculate the estimated DC output, the 95% conﬁdence interval of the 
+#*    response and the 95% prediction interval with a wind velocity of one and 
+#*    ten metres per second. Comment your results.
+#*------------------------------------------------------------------------------
+#   wind velocities of interest
+WV.new2 <- data.frame(WindVelocity.inv=c(1/1,1/10))
+
+#   estimated DC output and confidence interval of the response
+WV.conf <- predict(mod.inv, newdata=WV.new2, interval="confidence", level=0.95);   WV.conf
+
+#   prediction interval
+WV.pred <- predict(mod.inv, newdata=WV.new2, interval="prediction", level=0.95);   WV.pred
+
+#*   REMARKS: With a wind velocity of 10 m/s we obtain an estimated DC output of 
+#*            1.427314 A. The confidence interval of the response is 
+#*            [1.386768, 1.467861] and the slightly larger prediction interval 
+#*            is [1.228331, 1.626298].
+#*            On the other hand with a wind velocity of 1 m/s we obtain a 
+#*            negative estimated DC output which is useless.
+#*            The reason for this is that we are extrapolating outside the 
+#*            interval of the measured wind velocities.
+#*            In Prob. 7.5.2.d we calculated the minimal wind velocity necessary
+#*            to produce a DC output which was considerably bigger than 1 m/s.
