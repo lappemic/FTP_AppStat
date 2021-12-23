a791f40d2a9d403fe1d7525fcb0c35cb1d44d7a1
lappemic <61876623+lappemic@users.noreply.github.com>
2021-12-23 15:33:36 +0100

    Preparing Week 1

--- Week_8-Multiple_Linear_Regression.R
+++ Week_8-Multiple_Linear_Regression.R
@@ -27,3 +27,30 @@ rm(list=ls(all=TRUE))
 #*    Perform a complete residual analysis for both variants. Report all your 
 #*    ﬁndings and conclusions, compare with the model in Eqn. 8.5.a.
 #*------------------------------------------------------------------------------
+
+
+#*------------------------------------------------------------------------------
+#* a. Fit the simple linear model
+#*  
+#*    Time = β_0 + β_1 * Volume + ε
+#* 
+#*    to the data.
+#*------------------------------------------------------------------------------
+
+
+#*------------------------------------------------------------------------------
+#* b. Perform a complete residual analysis assuming that the errors are 
+#*    independent, normally distributed with expected value zero and constant 
+#*    variance. Report all your ﬁndings and conclusions.
+#*------------------------------------------------------------------------------
+
+
+#*------------------------------------------------------------------------------
+#* c. Apply the following Tukey’s ﬁrst aid transformation:
+#* 
+#*      log(Time) = β_0 + β_1 * log(Volume) + ε
+#*      log(Time) = β_0 + β_1 * √ Volume + ε
+#*    
+#*    Perform a complete residual analysis for both variants. Report all your 
+#*    ﬁndings and conclusions, compare with the model in Eqn. 8.5.a.
+#*------------------------------------------------------------------------------
