2c336be0a610618b63f4dde79326733db3ea1bfb
lappemic <61876623+lappemic@users.noreply.github.com>
2022-01-20 15:13:20 +0100

    8.5.4

--- Week_8-Multiple_Linear_Regression.R
+++ Week_8-Multiple_Linear_Regression.R
@@ -348,7 +348,7 @@ par(op)
 #*    with a q-q plot. Repeat this several times to get an idea of the 
 #*    randomness of the deviations in the q-q plot.
 #*------------------------------------------------------------------------------
-
+par(mfrow=c(1,1))
 
 #*------------------------------------------------------------------------------
 #* a. Normal distribution. Draw n = 10, 20, 50 and 100 random numbers from a 
@@ -361,7 +361,8 @@ set.seed(1)
 for(i in 1:100){
   x <- rnorm(n, mean=0, sd=1)
   #   q-q plot
-  qqnorm(x, pch=20, xlim=10*c(-1,1), ylim=10*c(-1,1), 
+  par(mfrow=c(1,1))
+    qqnorm(x, pch=20, xlim=10*c(-1,1), ylim=10*c(-1,1), 
          main="q-q plot: Standard Normal Distribution")
   qqline(x, col="red")
   grid()
@@ -381,6 +382,7 @@ set.seed(1)
 for(i in 1:50){
   x <- rt(n, df=dfree)
   #   q-q plot
+  par(mfrow=c(1,1))
   qqnorm(x, pch=20, xlim=10*c(-1,1), ylim=10*c(-1,1), main="q-q plot: Student's t Distribution")
   qqline(x, col="red")
   grid()
@@ -401,6 +403,7 @@ set.seed(1)
 for(i in 1:50){
   x <- rchisq(n, df=dfree)
   #   q-q plot
+  par(mfrow=c(1,1))
   qqnorm(x, pch=20, xlim=10*c(-1,1), ylim=10*c(-1,1), main="q-q plot: chi^2 Distribution")
   qqline(x, col="red")
   grid()
@@ -424,4 +427,245 @@ for(i in 1:50){
 #* 
 #* Perform a complete residual analysis for the variants. 
 #* Report all your ﬁndings and conclusions. Which model is the best?
+#*******************************************************************************
+path <- file.path("04_Datasets", "windmill.dat")
+data <- read.table(path, header=TRUE)
+str(data)
+
+#   define new variables
+data$WindVelocity.log <- log10(data$WindVelocity)
+data$WindVelocity.inv <- 1/data$WindVelocity
+data$DCOutput.log     <- log10(data$DCOutput)
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   (1) Naiv Model
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   Estimation of the parameters
+mod <- lm(DCOutput ~ WindVelocity, data)
+summary(mod)
+
+#   Scatter diagram: DCOutput ~ WindVelocity
+par(mfrow=c(1,1))
+plot(DCOutput ~ WindVelocity, data, pch=20, xlim=c(5,25), ylim=c(0,3), 
+     main="DCOutput ~ WindVelocity")
+grid()
+abline(mod)
+
+#   REMARK: It is obvious that the straight line does not appropriately fit the curved data.
+
+#   residual analysis
+op <- par(mfcol=c(2,3))
+#   Tukey-Anscombe plot
+plot(mod, which=1, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod, which=1, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   scale-location plot
+plot(mod, which=3, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod, which=3, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   q-q plot
+plot(mod, which=2, pch=20)
+grid()
+plot.lmSim(mod, which=2, SEED=1)
+grid()
+par(op)
+
+#   REMARKS:
+#   1.  Tukey-Anscombe plot shows very curved structure.
+#       In the simulation it is visible that the original curve is extreme.
+#       => The expected value of the residuals cannot be constant.
+#   2.  Scale-location plot shows a weak downward trend.
+#       In the simulation it is visible that the original curve is within the 19 
+#       simulated curves.
+#       => There is no hint that the scattering of the residuals is not constant.
+#   3.  q-q plot shows no obvious discrepancy.
+#       => There is no hint that the residuals are not normally distributed.
+
+#   CONCLUSION: The fit is not satisfactory at all. 
+#               Try log-transformations of response and explanatory variable.
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   (2) Model with First Aid Transformations of Response and Explanatory Variable
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   Estimation of the parameters
+mod.loglog <- lm(DCOutput.log ~ WindVelocity.log, data)
+summary(mod.loglog)
+
+#   Scatter diagram: DCOutput ~ WindVelocity
+par(mfrow=c(1,1))
+plot(DCOutput.log ~ WindVelocity.log, data, pch=20, xlim=log10(c(5,25)), ylim=log10(c(0.1,3)), main="log(DCOutput) ~ log(WindVelocity)")
+grid()
+abline(mod.loglog)
+#identify(data$WindVelocity.log, data$DCOutput.log)
+##  25
+
+#   REMARK: It is obvious that the straight line does not appropriately fit the 
+#           curved data.
+#           Especially the observation with index i=25 is extremely far away 
+#           from the straight line and will be marked as an outlier in the 
+#           following residual analysis.
+
+#   residual analysis
+op <- par(mfcol=c(2,3))
+#   Tukey-Anscombe plot
+plot(mod.loglog, which=1, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.loglog, which=1, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   scale-location plot
+plot(mod.loglog, which=3, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.loglog, which=3, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   q-q plot
+plot(mod.loglog, which=2, pch=20)
+grid()
+plot.lmSim(mod.loglog, which=2, SEED=1)
+grid()
+par(op)
+
+#   REMARKS:
+#   1.  Tukey-Anscombe plot shows very curved structure.
+#       In the simulation it is visible that the original curve is extreme.
+#       => The expected value of the residuals cannot be constant.
+#   2.  Scale-location plot shows an oscillating behavior.
+#       In the simulation it is visible that the original curve is somehow 
+#       different from the 19 simulated curves.
+#       => The scattering of the residuals is not constant.
+#   3.  q-q plot shows no obvious discrepancy. Observation with index i=25 is 
+#       marked as an outlier.
+#       => The residuals are not normally distributed.
+
+#   CONCLUSION: The fit is not satisfactory at all, all three diagnostic tools 
+#               show abnormalities.
+#               => The first aid transformations were not helpful.
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   (3) Mechanistic Model based on First Principles
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   Estimation of the parameters
+mod.inv <- lm(DCOutput ~ WindVelocity.inv, data)
+summary(mod.inv)
+
+#   Scatter diagram: DCOutput ~ WindVelocity
+par(mfrow=c(1,1))
+plot(DCOutput ~ WindVelocity.inv, data, pch=20, xlim=1/(c(5,25)), ylim=c(0,3), main="DCOutput ~ 1/WindVelocity")
+grid()
+abline(mod.inv)
+
+#   REMARK: Nice fit!
+
+#   residual analysis
+op <- par(mfcol=c(2,3))
+#   Tukey-Anscombe plot
+plot(mod.inv, which=1, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.inv, which=1, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   scale-location plot
+plot(mod.inv, which=3, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.inv, which=3, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   q-q plot
+plot(mod.inv, which=2, pch=20)
+grid()
+plot.lmSim(mod.inv, which=2, SEED=1)
+grid()
+par(op)
+
+#   REMARKS:
+#   1.  Tukey-Anscombe plot shows weak curved structure.
+#       In the simulation it is visible that the original curve is not extreme.
+#       => There is no hint that the expected value of the residuals is not constant.
+#   2.  Scale-location plot shows a hick.
+#       In the simulation it is visible that the original curve is within 19 simulated curves.
+#       => There is no hint that the scattering of the residuals is not constant.
+#   3.  q-q plot shows no obvious discrepancy except maybe an indication of a 
+#       short-tailed distribution at the right end.
+#       (Short-tailed distribution of the errors is in general not critical for 
+#       least-squares estimation.)
+#       => There is no hint that the residuals are not normally distributed.
+
+#   CONCLUSION: The fit is satisfactory, all three diagnostic tools show no abnormalities.
+#               => The mechanistic model is the best of all three.
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   Correlation of the Residuals
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   WARNING: We absolutely have NO idea if the chronological order in the data 
+#             set "windmill.dat" has never been changed.
+#            At least there is no obvoius order visible in the data set.
+#            => We will look at the correlation of the residuals assuming that 
+#             in the data set "windmill.dat" we still have the original order.
+op <- par(mfcol=c(2,3))
+ts0 <- residuals(mod)
+max0 <- max(abs(range(ts0)))
+plot(ts0, ylim=max0*c(-1,1), type="h", xlab="Index i", ylab="Residuals", 
+     main="DCOutput ~ WindVelocity")
+grid()
+lines(ts0, type="h")
+abline(h=0)
+#   correlation:  R[t+1] ~ R[t]
+plot(ts0[-length(ts0)], ts0[-1], pch=20, xlim=max0*c(-1,1), ylim=max0*c(-1,1), xlab="e[i]", ylab="e[i+1]")
+grid()
+
+ts1 <- residuals(mod.loglog)
+max1 <- max(abs(range(ts1)))
+plot(ts1, ylim=max1*c(-1,1), type="h", xlab="Index i", ylab="Residuals", 
+     main="log(DCOutput) ~ log(WindVelocity)")
+grid()
+lines(ts1, type="h")
+abline(h=0)
+#   correlation:  R[t+1] ~ R[t]
+plot(ts1[-length(ts1)], ts1[-1], pch=20, xlim=max1*c(-1,1), ylim=max1*c(-1,1), 
+     xlab="e[i]", ylab="e[i+1]")
+grid()
+
+ts2 <- residuals(mod.inv)
+max2 <- max(abs(range(ts2)))
+plot(ts2, ylim=max2*c(-1,1), type="h", xlab="Index i", ylab="Residuals", main="DCOutput ~ 1/WindVelocity")
+grid()
+lines(ts2, type="h")
+abline(h=0)
+#   correlation:  R[t+1] ~ R[t]
+plot(ts2[-length(ts2)], ts2[-1], pch=20, xlim=max2*c(-1,1), ylim=max2*c(-1,1), xlab="e[i]", ylab="e[i+1]")
+grid()
+par(op)
+
+#   REMARKS: All models show uncorrelated residuals.
+
+
+#*******************************************************************************
+#* Problem 8.5.5 (Atmospheric Pressure versus Boiling Point of Water)
+#* We come back to Prob. 7.5.3. Investigate the following regression model
+#* 
+#*  Boiling = β_0 + β_1 * log(Pressure) + ε
+#*  
+#*  in connection with the complete data set. Perform a complete residual 
+#*  analysis. Report all your ﬁndings and conclusions.
 #*******************************************************************************
