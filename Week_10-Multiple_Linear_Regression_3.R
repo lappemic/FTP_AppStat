cb047406e332342deb0c9cf178dc61fec1d8cb5b
lappemic <61876623+lappemic@users.noreply.github.com>
2022-01-21 01:44:13 +0100

    10.5.3

--- Week_10-Multiple_Linear_Regression_3.R
+++ Week_10-Multiple_Linear_Regression_3.R
@@ -192,3 +192,139 @@ par(op)
 #       Some observations have a leverage which is bigger than 0.2.
 #       Observations with index i=1 and 20 are again marked as influential observations.
 
+
+
+#*******************************************************************************
+#* Problem 10.5.3 (Robust Regression, cf. [32], Reg4, Problem 3). 
+#* Study Example 10.4.4 Syntetic data to illustrate lmrob.R. The data set is 
+#* lmrob-syntetic.dat. You need the instal the R-packages robustbase and rgl.
+#*******************************************************************************
+#   load package
+require(robustbase)
+require(rgl)
+
+path <- file.path("04_Datasets", "lmrob-syntetic.dat")
+data <- read.table(path, header=TRUE)
+str(data)
+summary(data)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   Least-squares estimation
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+mod.ls <- lm(y ~ x1 + x2, data)
+summary(mod.ls)
+
+#   diagnostic tools
+op <- par(mfcol=c(2,4))
+#   Tukey-Anscombe plot
+plot(mod.ls, which=1, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.ls, which=1, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   scale-location plot
+plot(mod.ls, which=3, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.ls, which=3, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   q-q plot
+plot(mod.ls, which=2, pch=20)
+grid()
+plot.lmSim(mod.ls, which=2, SEED=1)
+grid()
+
+#   residuals against leverages
+plot(mod.ls, which=5, pch=20)
+grid()
+abline(h=0, lty=3)
+par(op)
+
+#   REMARKS: All the diagnostic plots do not show any suspicious observations.
+#            => Perfect model!
+
+#   B U T:   There are ouliers which are visible in the following 3D-plot.
+
+#   3D-plot
+open3d()
+options(rgl.printRglwidget = TRUE)
+plot3d(x=data$x1,
+       y=data$x2,
+       z=data$y,
+       radius=1,
+       type="s",
+       col="red")
+#   add least-squares estimator
+x1.new <- seq(10, 30, length=101)
+x2.new <- seq(-20, 5, length=101)
+data.new <- expand.grid(data.frame(x1=x1.new, x2=x2.new))
+mod.ls.pred <- predict(mod.ls, newdata=data.new)
+z.col <- (mod.ls.pred-min(mod.ls.pred)) / diff(range(mod.ls.pred))
+col <- rainbow(200)[z.col*200+1]
+surface3d(x=x1.new,
+          y=x2.new,
+          z=matrix(mod.ls.pred, ncol=101, nrow=101),
+          col=col,
+          axes=F)
+
+#   Conclusion: The model is completely wrong!
+#               => Use robust regression instead.
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   robust MM-estimation
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+require(robustbase)
+require(rgl)
+mod.rob <- lmrob(y ~ x1 + x2, data)
+summary(mod.rob)
+
+#   3D-plot
+open3d()
+options(rgl.printRglwidget = TRUE)
+plot3d(x=data$x1,
+       y=data$x2,
+       z=data$y,
+       radius=1,
+       type="s",
+       col="red")
+#   add robust MM-estimator
+x1.new <- seq(10, 30, length=101)
+x2.new <- seq(-20, 5, length=101)
+data.new <- expand.grid(data.frame(x1=x1.new, x2=x2.new))
+mod.rob.pred <- predict(mod.rob, newdata=data.new)
+z.col <- (mod.rob.pred-min(mod.rob.pred)) / diff(range(mod.rob.pred))
+col <- rainbow(200)[z.col*200+1]
+surface3d(x=x1.new,
+          y=x2.new,
+          z=matrix(mod.rob.pred, ncol=101, nrow=101),
+          col=col,
+          axes=F)
+
+#   diagnostic tools
+op <- par(mfcol=c(2,3))
+plot(mod.rob)
+par(op)
+
+#   REMARKS: The first diagram at the top left replaces the classic diagram residuals versus leverages.
+#            There are clearly 2*4=8 outliers visible in all diagrams.
+#            7 of them are also leverage points.
+#            These 8 outliers are already listed in the summary output as outliers.
+#            These 8 outliers can also be seen in the 3D-plot.
+#            If there are more than two explanatory variables in a model we cannot rely on 3D-plots anymore!
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   Least-squares estimation without outliers
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+ind.out <- c(7,17,27,37,47,57,67,77)
+mod.ls.red <- lm(y ~ x1 + x2, data, subset=-ind.out)
+summary(mod.ls.red)
+
+#   REMARK: This is more or less the same result as with the robust MM-regression.
+
+
