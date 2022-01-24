d07dfb1c9fc860a29ea57783c003633ba7118702
lappemic <61876623+lappemic@users.noreply.github.com>
2022-01-24 11:31:58 +0100

    13.5.2

--- Week_13-Factorial_Design_and_Analysis_of_Variance.R
+++ Week_13-Factorial_Design_and_Analysis_of_Variance.R
@@ -102,23 +102,117 @@ par(op)
 
 
 #*******************************************************************************
-#* Problem 13.5.1 (Compression of Single-Wall Containers, cf. [9], Example 2.14). 
-#* The article Compression of Single-Wall Corrugated Shipping Containers Using 
-#* Fixed and Floating Test Platens in the Journal of Testing and Evaluation, 
-#* 1992, p. 318-320, describes an experiment in which several diﬀerent types of 
-#* boxes were compared with respect to compression strength.
+#* Problem 13.5.2 (Energy Consumption of Dehumidiﬁer, cf. [32], DoE2, Problem 3)
+#* A consumer protection organisation compares the annual energy consumption of 
+#* ﬁve diﬀerent brands of dehumidiﬁers. Because energy consumption depends on the 
+#* current humidity, each brand has been tested at four diﬀerent humidity levels 
+#* ranging from moderate to high humidity. For each brand, therefore, four devices 
+#* were randomly assigned to humidity levels. The mean energy consumption resulting 
+#* from the experiment [in kWh] is recorded in the following table:
 #* 
-#*      *Data Table in Script*
-#*      
+#*                              *Table in script*
+#*                              
 #*******************************************************************************
-#* a. Enter the data yourself in R and represent it with notched box-plots. 
-#*    You need to deﬁne a factor (Box) for the type of box.
-#* b. Is there a diﬀerence between diﬀerent types of boxes? Perform a statistical 
-#*    hypothesis test on the 5% level.
+#* a. Enter the data yourself in R. You need to deﬁne two factors Brand and Humidity.
+#* b. Is there a diﬀerence in energy consumption between the ﬁve diﬀerent brands? 
+#*    Perform a statistical hypothesis test on the 5% level.
+#* c. Is there a diﬀerence in the diﬀerent humidity levels? Perform a statistical 
+#*    hypothesis test on the 1% level. Do your ﬁndings support the idea to use the 
+#*    block factor Humidity?
+#* d. Perform a residual analysis.
+#* e. Check the additivity of the model with an interaction plot.
 #*------------------------------------------------------------------------------
-
-
-path <- file.path("04_Datasets", ".dat")
-data <- read.table(path, header=TRUE)
+#* Creating data frame
+data <- data.frame(Energy  = c(685,792,838,875,
+                               722,806,893,953,
+                               733,802,880,941,
+                               811,888,952,1005,
+                               828,920,978,1023),
+                   Brand    = paste0("B",rep(1:5,rep(4,5))),
+                   Humidity = paste0("H",rep(1:4,5)))
+data$Brand <- as.factor(data$Brand)
+data$Humidity <- as.factor(data$Humidity)
 str(data)
-summary(data)
+data##    'data.frame':   20 obs. of  3 variables:
+##     $ Energy  : num  685 792 838 875 722 806 893 953 733 802 ...
+##     $ Brand   : Factor w/ 5 levels "B1","B2","B3",..: 1 1 1 1 2 2 2 2 3 3 ...
+##     $ Humidity: Factor w/ 4 levels "H1","H2","H3",..: 1 2 3 4 1 2 3 4 1 2 ...
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   (b,c) ANOVA
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+mod <- aov(Energy ~ Brand + Humidity, data)
+summary(mod)
+
+
+#   REMARKS: The P-value of the factor "Brand" is 5.42e-09 < 0.05 and therefore we reject the null hypothesis.
+#            This means that there significant differences in energy consumation between the different brands
+#            The P-value of the block factor "Humidity" is 2.36e-11 < 0.01 and therefore we reject the null hypothesis.
+#            We therefore conclude that it was important to use "Humidity" as a block factor.
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   (d) Residual Analysis
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   Residual analysis (to be able to use plot.lmSim we need to fit the model with lm())
+mod.lm <- lm(Energy ~ Brand + Humidity, data)
+summary(mod.lm)
+
+anova(mod.lm)
+
+#   REMARK: This is the same table as in (b) and (c).
+
+#   diagnostic tools
+op <- par(mfcol=c(2,3))
+#   Tukey-Anscombe plot
+plot(mod.lm, which=1, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.lm, which=1, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   scale-location plot
+plot(mod.lm, which=3, pch=20)
+grid()
+abline(h=0, lty=3)
+plot.lmSim(mod.lm, which=3, SEED=1)
+grid()
+abline(h=0, lty=3)
+
+#   q-q plot
+plot(mod.lm, which=2, pch=20)
+grid()
+plot.lmSim(mod.lm, which=2, SEED=1)
+grid()
+par(op)
+
+#   REMARK: Nice model!
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+#   (e) Interaction plot
+#--------------------------------------------------------------------------------------------------------------------------------------------------
+op <- par(mfrow=c(1,2))
+interaction.plot(x.factor=data$Brand, trace.factor=data$Humidity, response=data$Energy, fun=mean, ylim=c(600,1100),
+                 xlab="Brand", trace.label="Humidity", ylab="Mean of Energy Consumation",
+                 main="Interaction Plot")
+abline(v=data$Humidity, lty=3, col="gray")
+text(data$Brand, data$Energy, label=data$Humidity, pos=3)
+
+interaction.plot(x.factor=data$Humidity, trace.factor=data$Brand, response=data$Energy, fun=mean, ylim=c(600,1100),
+                 xlab="Humidity", trace.label="Brand", ylab="Mean of Energy Consumation",
+                 main="Interaction Plot")
+abline(v=data$Brand, lty=3, col="gray")
+text(data$Humidity, data$Energy, label=data$Brand, pos=3)
+par(op)
+
+#   REMARKS: The polygonal lines are more or less parallel
+#            => Additive model is appropriate.
+#            It can also be seen that energy consumption increases with increasing humidity.
+#            Interaction can not be formally tested because we do not have repeated measures, 
+#             i.e. we have too few observations.
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------
