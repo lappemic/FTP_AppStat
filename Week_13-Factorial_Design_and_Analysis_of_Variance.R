1dfdc165b1622ea078673d5a8d15982b9deaf10e
lappemic <61876623+lappemic@users.noreply.github.com>
2022-01-24 11:31:37 +0100

    13.5.2

--- Week_13-Factorial_Design_and_Analysis_of_Variance.R
+++ Week_13-Factorial_Design_and_Analysis_of_Variance.R
@@ -101,7 +101,21 @@ par(op)
 #   REMARK: Nice model!
 
 
-#--------------------------------------------------------------------------------------------------------------------------------------------------
+#*******************************************************************************
+#* Problem 13.5.1 (Compression of Single-Wall Containers, cf. [9], Example 2.14). 
+#* The article Compression of Single-Wall Corrugated Shipping Containers Using 
+#* Fixed and Floating Test Platens in the Journal of Testing and Evaluation, 
+#* 1992, p. 318-320, describes an experiment in which several diﬀerent types of 
+#* boxes were compared with respect to compression strength.
+#* 
+#*      *Data Table in Script*
+#*      
+#*******************************************************************************
+#* a. Enter the data yourself in R and represent it with notched box-plots. 
+#*    You need to deﬁne a factor (Box) for the type of box.
+#* b. Is there a diﬀerence between diﬀerent types of boxes? Perform a statistical 
+#*    hypothesis test on the 5% level.
+#*------------------------------------------------------------------------------
 
 
 path <- file.path("04_Datasets", ".dat")
